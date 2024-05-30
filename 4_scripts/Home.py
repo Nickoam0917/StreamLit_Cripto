5e57f67e7e4fc01a8f1599a1d10dec390346d3b9
Nicolas Margarido <162373061+Nickoam0917@users.noreply.github.com>
2024-05-30 17:08:03 -0300

    Update Home.py

--- 4_scripts/Home.py
+++ 4_scripts/Home.py
@@ -1,13 +1,20 @@
 import streamlit as st
 import pandas as pd
 from PIL import Image
+import sqlite3
 
 # Lendo o caminho do banco de dados a partir dos segredos
 db_path = st.secrets["database"]["path"]
 
 def main():
+    # Estabelecer conexão com o banco de dados SQLite
+    conn = sqlite3.connect(db_path)
+
     # Ler os dados do banco de dados usando pandas
-    df_yahoo_cotacao = pd.read_sql("SELECT * FROM cotacao_yahoo.db;", f"sqlite:///{db_path}")
+    df_yahoo_cotacao = pd.read_sql("SELECT * FROM cotacao_yahoo.db;", conn)
+
+    # Fechar a conexão com o banco de dados
+    conn.close()
 
     # Personalizar o título da página
     st.set_page_config(
@@ -35,4 +42,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
